85c37fb2f5fd34293d040765b535daa22c639d87
Frederic Chan <frederic.t.chan@gmail.com>
2018-09-30 14:25:39 +0800

    first commit

---
+++ always-online.py
@@ -0,0 +1,53 @@
+import time
+import os
+
+import schedule
+from selenium import webdriver
+
+import credentials
+
+lyric_generator = None
+
+
+def get_lyric():
+    lyrics = []
+    n = 0
+    with open('lyrics.txt') as f:
+        for line in f:
+            lyrics.append(line[0:len(line) - 1])
+    while True:
+        yield lyrics[n % len(lyrics)]
+        n += 1
+
+
+def check_and_login():
+    """
+
+    :return:
+    """
+    # Show lyric
+    global lyric_generator
+    if lyric_generator is None:
+        lyric_generator = get_lyric()
+    print(next(lyric_generator))
+
+    # Watch website
+    exit_code = os.system('ping www.qq.com -c 1 -t 3')
+    if exit_code:
+        print('Network disconnected. Try to login.')
+        driver = webdriver.Chrome()
+        driver.get('http://172.16.0.101/')
+        time.sleep(3)
+        driver.find_element_by_id('username').send_keys(credentials.username)
+        driver.find_element_by_id("pwd_tip").click()
+        driver.find_element_by_id("pwd").send_keys(credentials.password)
+        driver.find_element_by_id("loginLink").click()
+
+
+check_and_login()
+
+schedule.every(30).seconds.do(check_and_login)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
